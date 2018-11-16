5bc6be1a86bbf0a991538e92252c250bba702843
Richard Pawson <rpawson@nakedobjects.net>
2018-11-16 18:12:51 +0000

    Version 1.0.0-beta01

---
+++ ConsolePlus/ConsolePlus.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quadrivia
+{
+    public static class ConsolePlus
+    {
+        private const string Invalid = "Invalid input";
+
+        //Requests the user to enter a number via the console
+        public static int ReadInteger(string prompt, int min = 0, int max = int.MaxValue)
+        {
+            int value = 0;
+            while (true)
+            {
+                Console.Write(prompt);
+                try
+                {
+                     value = Convert.ToInt32(Console.ReadLine());
+                    if (value >= min && value <= max) break;
+                }
+                catch (Exception)
+                {
+                }
+                Console.WriteLine(Invalid);
+            }
+            return value;
+        }
+
+        public static int ReadInteger(string prompt, IEnumerable<int> validValues)
+        {
+            int value = 0;
+            while (true)
+            {
+                Console.Write(prompt);
+                try
+                {
+                    value = Convert.ToInt32(Console.ReadLine());
+                    if (validValues.Contains(value)) break;
+                }
+                catch (Exception) { }
+                Console.WriteLine(Invalid);
+            }
+            return value;
+        }
+
+        public static T ReadType<T>(string prompt)
+        {
+            T value;
+            var convertor = TypeDescriptor.GetConverter(typeof(T));
+            while (true)
+            {
+                Console.Write(prompt);
+                try
+                {
+                    string input = Console.ReadLine();
+                    value = (T)(convertor.ConvertFromInvariantString(input));
+                    break;
+                }
+                catch (Exception) { }
+                Console.WriteLine(Invalid);
+            }
+            return value;
+        }
+        //Requests the user to enter a number via the console
+        public static T ReadType<T>(string prompt, T min, T max) where T : IComparable
+        {
+            T value;
+            var convertor = TypeDescriptor.GetConverter(typeof(T));
+            while (true)
+            {
+                Console.Write(prompt);
+                try
+                {
+                    string input = Console.ReadLine();
+                    value = (T)(convertor.ConvertFromInvariantString(input));                 
+                    if (value.CompareTo(min) >=0 && value.CompareTo(max) <= 0) break;
+                }
+                catch (Exception) {}
+                Console.WriteLine(Invalid);
+            }
+            return value;
+        }
+
+        public static T ReadType<T>(string prompt,IList<T> validValues)
+        {
+            T value;
+            var convertor = TypeDescriptor.GetConverter(typeof(T));
+            while (true)
+            {
+                Console.Write(prompt);
+                try
+                {
+                    string input = Console.ReadLine();
+                    value = (T)(convertor.ConvertFromInvariantString(input));
+                    if (validValues.Contains(value))
+                    {
+                        break;
+                    }
+                }
+                catch (Exception) { }
+                Console.WriteLine(Invalid);
+            }
+            return value;
+        }
+
+        public static string ReadString(string prompt, int minLength = 0, int maxLength = int.MaxValue)
+        {
+            string value = "";
+            while (true)
+            {
+                try
+                {
+                    Console.Write(prompt);
+                    value = Console.ReadLine();
+                    if (value.Length >= minLength && value.Length <= maxLength) break;
+                }
+                catch (Exception) { }
+                Console.WriteLine(Invalid);
+            }
+            return value;
+        }
+
+        public static int ReadIndex(string prompt, IList<string> validValues, bool caseSensitive = false, bool ignoreInvalid = true)
+        {
+            string value = "";
+            int index = 0;
+            if (!caseSensitive)
+            {
+                validValues = validValues.Select(v => v.ToUpper()).ToList();
+            }
+            while (true)
+            {
+                try
+                {
+                    Console.Write(prompt);
+                    value = Console.ReadLine();
+                    var value2 = value;
+                    if (!caseSensitive)
+                    {
+                        value2 = value.ToUpper();
+                    }
+                    if (validValues.Contains(value2))
+                    {
+                        index = validValues.IndexOf(value2);
+                        break;
+                    }
+                }
+                catch (Exception) { }
+                Console.WriteLine(Invalid);
+            }
+            return index;
+        }
+
+        public static int ReadCharIndex(string prompt, IList<char> validValues, bool caseSensitive = false)
+        {
+            Console.Write(prompt);
+            int index = 0;
+            char value = '\0';
+            if (!caseSensitive)
+            {
+                validValues = validValues.Select(v => Char.ToUpper(v)).ToList();
+            }
+            while (true)
+            {
+                    value = Console.ReadKey(true).KeyChar;
+                var value2 = value;
+                    if (!caseSensitive)
+                    {
+                        value2 = Char.ToUpper(value);
+                    }
+                if (validValues.Contains(value2))
+                {
+                    index = validValues.IndexOf(value2);
+                    break;
+                }
+            }
+            Console.WriteLine(value);
+            return index;
+        }
+
+        public static void WriteList<T>(IEnumerable<T> list, string spacer = ", ")
+        {
+            if (list.Count() == 0) return;
+            foreach (var item in list.Take(list.Count() - 1))
+            {
+                Console.Write(item.ToString() + spacer);
+            }
+            Console.Write(list.Last());
+            Console.WriteLine();
+        }
+
+        public static void WriteValues<T>(params T[] list) 
+        {
+            WriteList(list);
+        }
+
+    }
+}
---
+++ ConsoleTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Quadrivia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = ConsolePlus.ReadType("Int in range 3-7: ", 3, 7);
+            Console.WriteLine(a);
+            float b = ConsolePlus.ReadType("Float in range 3-7: ", 3.0f, 7.0f);
+            Console.WriteLine(b);
+            decimal c = ConsolePlus.ReadType("Decimal in range 3-7: ", 3.0m, 7.0m);
+            Console.WriteLine(c);
+            double d = ConsolePlus.ReadType("Double in range 3-7: ", 3.0, 7.0);
+            Console.WriteLine(d);
+            int e = ConsolePlus.ReadType("Int from 4,5,9: ", new int[] { 4, 5, 9 });
+            Console.WriteLine(e);
+            string f = ConsolePlus.ReadString("String of 3-7 chars: ", 3, 7);
+            Console.WriteLine(f);
+            int g = ConsolePlus.ReadIndex("String from Foo, Bar (case insensitive): ", new string[] { "Foo", "Bar" });
+            Console.WriteLine(g);
+            int h = ConsolePlus.ReadIndex("String from Foo, Bar (case sensitive): ", new string[] { "Foo", "Bar" }, true);
+            Console.WriteLine(h);
+            int i = ConsolePlus.ReadCharIndex("Char from Y, N (case insensitive): ", new char[] { 'Y', 'N' }, false);
+            Console.WriteLine(i);
+            int j = ConsolePlus.ReadCharIndex("Char from Y, N (case sensitive): ", new char[] { 'Y', 'N' }, true);
+            Console.WriteLine(j);
+
+            Console.WriteLine("Testing output functions");
+            ConsolePlus.WriteList(new int[] { 3, 1, 4, 1, 5, 9 });
+            ConsolePlus.WriteList(new int[] { 3, 1, 4, 1, 5, 9 }, "-");
+            ConsolePlus.WriteList(new int[] { 3, 1, 4, 1, 5, 9 }, "");
+            ConsolePlus.WriteValues(3, 1, 4, 1, 5, 9);
+            ConsolePlus.WriteValues("Hello","World");
+
+            Console.ReadKey();
+        }
+    }
+}
